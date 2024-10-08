90e75c712ba8ec0be30358599ec1cee7f11a1932
maol07001 <maol07001@E-PW05CGR4>
2024-10-08 14:37:45 +0200

    Add project files.

---
+++ Inför prov 2.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace uppgift_2._3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar vill du hyra bilen");
+            int x = int.Parse(Console.ReadLine());
+            int antaldagar = (x * 500) - 500;
+            int dagsum = 300 + antaldagar;
+
+            Console.WriteLine("Hur många km ska du köra?");
+            int km = int.Parse(Console.ReadLine());
+            int kilometer = km * 1;
+            int sum = dagsum + kilometer;
+
+            Console.WriteLine("summan är: " + sum + "kronor.");
+        }
+    }
+}
